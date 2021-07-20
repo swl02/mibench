cbe1683f0c5b1590acab087a52155d8373dea829
swl02 <soonweileong@gmail.com>
2021-07-20 22:06:29 +1000

    fixing instrumenting bug

--- security/rijndael/cal_chksum.py
+++ security/rijndael/cal_chksum.py
@@ -2,31 +2,81 @@
 counter = 0
 skip = 0
 checksum = 0
+chk_for_bb = []
+prev_inst = ""
+gap = 0
+
+#only branch for now
+def is_cfi(instruction,splitted_instruction):
+
+    if instruction.find('<') != -1 and len(splitted_instruction) == 5:
+        return True
+
+    return False
+
+def is_label(instruction,splitted_instruction):
+    if instruction.find('<') != -1 and len(splitted_instruction) == 2:
+        return True
+
+    return False
+
+def is_inst(instruction,splitted_instruction):
+    if instruction.find('<') != -1 and len(splitted_instruction) == 4:
+        return True
+
+    return False
+
+
 with open('aes.dump','r') as fp:
     stream = fp.readlines()
-    for line in stream:
-
+    for inst in stream:
+        
+        #skipping header
         if (skip < 6):
             skip = skip + 1
             continue
 
-        splitted_string = line.split()
+        if (skip == 6):
+            prev_inst = inst
+            skip = skip + 1
+            continue
 
-        #detect all instruction
-        if (len(splitted_string) == 4):
-            inst_code = int(splitted_string[1], 16)
+        splitted_string = prev_inst.split()
+        next_splitted_string = inst.split()
+
+        ## skipping newline
+        if (len(next_splitted_string) == 0):
+            continue        
+
+        # #detect all instruction
+        if (len(next_splitted_string) == 4 or len(next_splitted_string) == 5):
+            inst_code = int(next_splitted_string[1], 16)
             #doing checksum calculation
             upper_16 = inst_code >> 16
             lower_16 = inst_code & 0xffff
             checksum = checksum ^ lower_16 ^ upper_16
-                        
 
-        ##detect control flow instruction
-        if line.find('<') != -1:
-            print(line)
-            print(hex(checksum))
 
-        if counter == 10:
-            break
+        print("turn " + str(counter) + "\n")
+        print(splitted_string)
+        print(next_splitted_string)
 
+        if ((is_cfi(prev_inst,splitted_string) or is_label(prev_inst,splitted_string)) and (is_cfi(inst,next_splitted_string) or is_label(inst,next_splitted_string))) :
+
+            if ((is_cfi(prev_inst,splitted_string) and is_label(inst,next_splitted_string) and gap == 1) != True):
+                chk_for_bb.append(checksum)
+                checksum = 0
+                gap = 0
+                prev_inst = inst
+
+        # if counter == 20:
+        #     break
+
+        #debugging purpose
+        gap = gap + 1        
         counter = counter + 1
+
+with open('aes.chk','w') as fp:
+
+    for chk in chk_for_bb:        
+        fp.writelines(str(hex(chk)) + '\n')
--- security/rijndael/insert_inst.py
+++ security/rijndael/insert_inst.py
@@ -1,15 +1,49 @@
+def write_inst(inst ,new_inst):
+    return "\t.word\t"+ str(new_inst)[:-1] + "977" +"\n" + inst
+
+def is_cfi(inst):
+    if inst.find('.L') != -1 and inst.find(':') == -1:
+        return True
+    
+    return False
+
+def is_label(inst):
+    if inst.find(':') != -1:
+        return True
+    
+    return False
+
 counter = 0
+chk = []
+gap = 0
+prev_inst = ""
+
+with open('aes.chk','r') as fp:
+    chk = fp.readlines()
+
+print(len(chk))
+chk_counter = 0
 with open('aes.s','r') as fp:
     stream = fp.readlines()
 
-    for line in stream:
-        if line.find(':') != -1 or line.find('.L') != -1:
-            print('a')
-            print(line)
-            line = line + "chk\n"
-            stream[counter] = line
+    for inst in stream:
+        
+        if (chk_counter >= len(chk)):
+            break
 
-        counter = counter + 1 
+        if is_label(prev_inst):
+            inst = write_inst(inst,chk[chk_counter])
+            chk_counter = chk_counter + 1
+            stream[counter] = inst
+
+        if is_cfi(prev_inst) and not is_label(inst):
+            inst = write_inst(inst,chk[chk_counter])
+            chk_counter = chk_counter + 1
+            stream[counter] = inst
+
+        prev_inst = inst
+
+        counter = counter + 1
 
 
 with open('modified_aes.s','w') as fp:
