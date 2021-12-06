9ebebd8d69ea46d0719df1aaa2d045721c552ef1
swl02 <43950436+swl02@users.noreply.github.com>
2021-12-06 17:05:15 +1100

    Update llvm.sh

--- security/rijndael/llvm/llvm.sh
+++ security/rijndael/llvm/llvm.sh
@@ -3,6 +3,7 @@ clang --target=riscv64 -march=rv64g --sysroot=$RISCV/sysroot/usr aes.c aesxam.c 
 riscv64-unknown-elf-objdump -d  -M no-aliases aes.o > aes.dump
 riscv64-unknown-elf-objdump -d  -M no-aliases aesxam.o > aesxam.dump
 
+#the number after the file (assembly) is the starting value of placeholder as we are incrementing it.
 python init_layout.py aes 1 
 python init_layout.py aesxam 1000
 
@@ -10,7 +11,10 @@ clang --target=riscv64 -march=rv64g --sysroot=$RISCV/sysroot/usr init_aes.s init
 riscv64-unknown-elf-gcc -static -O0 -march=rv64g init_aes.o init_aesxam.o -o rijndael
 riscv64-unknown-elf-objdump -d -M no-aliases rijndael > rijndael.dump
 
+#the rijndael means the binary rijndael
 python cal_chksum.py rijndael
+
+#the rijndael means the rijndael.chk file
 python insert.py rijndael init_aes.s > modified_aes.s
 python insert.py rijndael init_aesxam.s > modified_aesxam.s
 
